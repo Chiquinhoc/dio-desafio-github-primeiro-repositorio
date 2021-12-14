5ecd7277ed221efae38881b26129ddce4a68dce0
Chiquinhoc <73004002+Chiquinhoc@users.noreply.github.com>
2021-12-14 10:19:11 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+	/**
+	 * Meu primeiro programa java
+	 */
+	public static void main(string[] args) {
+		//Mostra na tela o texto entre "..."
+		System.out.println("Este é nosso primeiro programa");
